dcca873d192c57739343dcc5eeaf055a1da5bc30
AltovoIta <cavila@fi.uba.ar>
2025-08-01 20:08:31 -0300

    feat: exercise 3.8 - DONE

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const morgan = require('morgan')
 
 
 const app = express()
-app.use(morgan('tiny'))
-app.use(express.json())
 
+morgan.token('data', (req) => {
+    return JSON.stringify(req.body)
+})
+
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+app.use(express.json())
 
 let persons = [
     { 
