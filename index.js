6f1e66ce688b087a82afe3e0b81c34c89fd7859d
AltovoIta <cavila@fi.uba.ar>
2025-08-01 19:01:09 -0300

    feat: exercise 3.5 - DONE

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 
 const app = express()
+app.use(express.json())
+
 
 let persons = [
     { 
@@ -54,10 +56,29 @@ app.get('/api/persons/:id', (request, response) => {
     
 })
 
+function generateId() {
+    return Math.floor(Math.random() * (2**32) )
+}
+
+app.post('/api/persons/', (request, response) => {
+
+    console.log("Creating ne2 person...", request.body)
+
+    const person = {
+        id: generateId(),
+        name: request.body.name,
+        number: request.body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
 app.delete('/api/persons/:id', (request, response) => {
     const personId = Number(request.params.id)
     console.log("Deleting id:", personId)
-    
+
     persons = persons.filter(person => person.id !== personId)
 
     console.log(`ID ${personId} deleted`)
