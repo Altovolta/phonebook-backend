9822a4dc62e57fb588771ab5c63bc84350404671
AltovoIta <cavila@fi.uba.ar>
2025-08-02 19:34:03 -0300

    feat: added buld scripts and added frontend build

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token('data', (req) => {
 })
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 app.use(express.json())
 
