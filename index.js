004f9a0969c7499a0d943d9bd0cb30424a325b5c
AltovoIta <cavila@fi.uba.ar>
2025-08-02 19:39:33 -0300

    feat: removed CORS - exercises 3.10 & 3.11 - DONE

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
-
 const app = express()
 
 morgan.token('data', (req) => {
     return JSON.stringify(req.body)
 })
 
-app.use(cors())
 app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 app.use(express.json())
