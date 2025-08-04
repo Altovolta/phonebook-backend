4a16a4793955248ed30f8531a3186b1dfce95cad
AltovoIta <cavila@fi.uba.ar>
2025-08-03 21:07:10 -0300

    feat: exercise 3.16 - DONE

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ let persons = [
     }
 ]
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
     const currentDate = new Date();
     const respMsg = `
         Phonebook has info for ${persons.length} people
@@ -47,14 +47,14 @@ app.get('/info', (request, response) => {
 })
 
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     Person.find({}).then(people => {
         response.json(people)
-    })
+    }).catch(error => next(error))
 }) 
 
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     const personId = Number(request.params.id)
     const person = persons.find(person => person.id === personId)
     if (person){
@@ -67,7 +67,7 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 
-app.post('/api/persons/', (request, response) => {
+app.post('/api/persons/', (request, response, next) => {
 
     const personData = request.body
 
@@ -91,28 +91,29 @@ app.post('/api/persons/', (request, response) => {
 
             person.save().then(savedPerson => {
                 response.json(savedPerson)
-            }).catch(error => {
-                console.error(error)
-                response.status(500).json({ error: 'Internal Server Error' })
-            })
+            }).catch(error => next(error))
         }
-    }).catch(error => {
-        console.error(error)
-        response.status(500).json({ error: 'Internal Server Error' })
-    })
+    }).catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
 
     Person.findByIdAndDelete(request.params.id)
     .then(result => {
         response.status(204).end()
-    }).catch(error => {
-        console.error(error)
-        response.status(500).json({ error: 'Internal Server Error' })
-    })
+    }).catch(error => next(error))
    
 })
+
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    response.status(500).json({ error: 'Internal Server Error' })
+}
+
+
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
